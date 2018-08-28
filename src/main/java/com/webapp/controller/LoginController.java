d205e70abd313a5c313168b0b5c349ec2f7638fe
QiQingLin7 <losertime7@gmail.com>
2018-08-28 12:52:15 +0800

    多余剔除

--- src/main/java/com/webapp/controller/LoginController.java
+++ src/main/java/com/webapp/controller/LoginController.java
@@ -3,16 +3,11 @@ package com.webapp.controller;
 import com.webapp.entity.User;
 import com.webapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 @RestController
-@SessionAttributes("a user")
 public class LoginController {
     private final UserService service;
 
@@ -28,11 +23,5 @@ public class LoginController {
         user.setPassword(password);
         User nameAndPassword = service.selectByNameAndPassword(user);
         return nameAndPassword != null;
-    }
-
-    @GetMapping("add")
-    public String Add(User user, BindingResult bindingResult) {
-
-        return "success";
     }
 }
