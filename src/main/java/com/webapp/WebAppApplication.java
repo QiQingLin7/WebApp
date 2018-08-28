69473430f6a02eedf5f6b431a66071cc86b144d3
QiQingLin7 <losertime7@gmail.com>
2018-08-28 11:55:18 +0800

    Bug Fix

--- src/main/java/com/webapp/WebAppApplication.java
+++ src/main/java/com/webapp/WebAppApplication.java
@@ -2,11 +2,18 @@ package com.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebAppApplication {
+public class WebAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebAppApplication.class, args);
     }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder;
+    }
+    
 }
